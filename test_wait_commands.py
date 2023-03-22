fcc7f7da6adacce632c3f3fea7aad2f91a070b8d
lukasribeiro <lucasmonribeiro@gmail.com>
2023-03-21 23:34:34 -0300

    add implicit wait

---
+++ test_wait_commands.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.implicitly_wait(12)
+
+browser.maximize_window()
+browser.get("https://chercher.tech/pratice/implicit-wait-example")
+
+checkbox = browser.find_element(By.XPATH, "//input[@type='checkbox']")
+assert checkbox.is_displayed()
+ 
