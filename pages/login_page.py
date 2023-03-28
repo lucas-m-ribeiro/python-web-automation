ac016e214d25a3d498446b00f0ebe5a2be921eea
lukasribeiro <lucasmonribeiro@gmail.com>
2023-03-27 23:42:36 -0300

    fix locators to constructor

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,8 +5,11 @@ class LoginPage:
 
     def __init__(self):
         self.driver = conftest.driver
+        self.user_name_field = (By.ID, "user-name")
+        self.password_field = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
     
     def fazer_login(self, usuario, senha):
-        self.driver.find_element(By.ID, "user-name").send_keys(usuario)
-        self.driver.find_element(By.ID, "password").send_keys(senha)
-        self.driver.find_element(By.ID, "login-button").click()
+        self.driver.find_element(*self.user_name_field).send_keys(usuario)
+        self.driver.find_element(*self.password_field).send_keys(senha)
+        self.driver.find_element(*self.login_button).click()
