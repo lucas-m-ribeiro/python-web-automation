a23cb4c26eb81fb92e9f0f05057fb020ee39e43f
Lukasribeiro <lucasmonribeiro@gmail.com>
2023-04-24 02:14:40 -0300

    new methods for framework

--- core/selenium.py
+++ core/selenium.py
@@ -1,4 +1,7 @@
 import conftest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains, Keys
 
 class Selenium:
 
@@ -22,3 +25,26 @@ class Selenium:
 
     def get_text_element(self, locator):
         return self.find_element(locator).text
+    
+    def wait_visible_el(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(*locator))
+    
+    def verify_element_not_exists(self, element):
+        assert len(self.find_element(element)) == 0, f"O elemento '{locator}' foi encontrado na tela, é esperado que ele não exista!"
+
+    def double_click(self, locator):
+        element = self.wait_visible_el(locator)
+        ActionChains(self.driver).double_click(element).perform()
+    
+    def click_right_button(self, locator):
+        element = self.wait_visible_el(locator)
+        ActionChains(self.driver).context_click(element).perform()
+
+    def press_key(self, locator, key):
+        element = self.find_element(locator)
+        if key == "ENTER":
+            element.send_keys(Keys.ENTER)
+        elif key == "SPACE":
+            element.send_keys(Keys.SPACE)
+        elif key == "F1":
+            element.send_keys(Keys.F1)
